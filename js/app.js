a959a28f0dc5022c429ccef9d9db6209ae7f2095
Rich Leonard <rich@homeimprovement-li.com>
2016-08-21 16:25:00 -0400

    Completed

--- js/app.js
+++ js/app.js
@@ -1,36 +1,54 @@
 $(document).ready(function() {
-    console.log('document.ready');
+
+    $(document).keydown(function() {
+        if (event.keyCode == 88) {
+            $('.ryu-cool').show();
+            $('.ryu-ready').hide();
+            $('.ryu-still').hide();
+            $('.ryu-throwing').hide();
+            console.log("x pressed");
+
+        }
+    });
+
+    $(document).keyup(function() {
+        if (event.keyCode == 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-ready').show();
+            $('.ryu-throwing').hide();
+            console.log("x pressed");
+
+        }
+    });
+
 
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
         $('.ryu-ready').show();
-    })
-
-    .mouseleave(function() {
+    }).mouseleave(function() {
         $('.ryu-ready').hide();
         $('.ryu-still').show();
-    })
-
-    .mousedown(function() {
-            console.log('mousedown');
-            playHadouken();
-            // play hadouken sound
-            $('.ryu-ready').hide();
-            $('.ryu-throwing').show();
-            $('.hadouken').finish().show()
-                .animate({ 'left': '1020px' },
-                    500,
-                    function() {
-                        $('.hadouken').hide();
-                        $('.hadouken').css('left', '520px');
-                    });
-        })
-        .mouseup(function() {
-            $('.ryu-throwing').hide();
-            $('.ryu-ready').show();
-            console.log('mouseup');
-            // ryu goes back to his ready position
-        });
+    }).mousedown(function() {
+        console.log('mousedown');
+        playHadouken();
+        // play hadouken sound
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show()
+            .animate({
+                    'left': '1020px'
+                },
+                500,
+                function() {
+                    $('.hadouken').hide();
+                    $('.hadouken').css('left', '520px');
+                });
+    }).mouseup(function() {
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+        console.log('mouseup');
+        // ryu goes back to his ready position
+    });
 });
 
 function playHadouken() {
